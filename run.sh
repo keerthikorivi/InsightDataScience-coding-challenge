865aaa25b1c0de067f27496feefdd0622c698526
keerthikorivi <keerthi.korivi@gmail.com>
2016-07-15 21:08:50 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@
 # but could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
-java -cp ./src/median_degree ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
+javac src/*.java
+java -cp src/ MedianDegree venmo_input/venmo-trans.txt venmo_output/output.txt
 
 
 
